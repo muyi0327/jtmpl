e5d34a8d9b8ac51056931c6dd970d406b7439cc8
muyi0327 <yfdezhuye@163.com>
2013-10-10 17:50:54 +0800

    Update template.js

--- template.js
+++ template.js
@@ -1,149 +1,157 @@
-(function(jtmpl, win) {
-    win.jtmpl = jtmpl();
-} (function() {
-    var jtmpl = {};
-    jtmpl.cache = {};
-    jtmpl.LEFT_DELIMITER = jtmpl.LEFT_DELIMITER || "<%";
-    jtmpl.RIGHT_DELIMITER = jtmpl.RIGHT_DELIMITER || "%>";
+(function(jtmpl, win){
+			win.jtmpl = jtmpl();
+		}(function(){
+			var jtmpl = {};
+			jtmpl.cache = {};
+			jtmpl.LEFT_DELIMITER = jtmpl.LEFT_DELIMITER || "<%";
+			jtmpl.RIGHT_DELIMITER = jtmpl.RIGHT_DELIMITER || "%>";
+			
+			var Ler = jtmpl.LEFT_DELIMITER, Rer = jtmpl.RIGHT_DELIMITER;
+			
+			
+			if(!String.prototype.trim){
+				String.prototype.trim = function(){
+				  return this.replace(/^[\s\xa0]+|[\s\xa0]+$/g, '');
+				}
+			}
+			
+			var _type = function(obj){
+				return /\[object\s+(\w+)/.exec(Object.prototype.toString.call(obj).toLowerCase())[1];
+			}
+			
+			var isObject = function(o){
+				var rst = Object.prototype.toString.call(o);
+				return rst == '[object Function]' || rst ==	 '[object Object]';
+			}
+			
+			// 编译模板并传递数据
+			jtmpl.template = function(tmpl, data){
+				if (!tmpl){
+					return;	
+				}
+				
+				var str = jtmpl.load(tmpl), rst = jtmpl.compile((str||'').trim(), data);
+				
+				return rst;
+			}
+			
+			// html过滤
+			jtmpl.encodeHTML = function(str){
 
-    var Ler = jtmpl.LEFT_DELIMITER,
-    Rer = jtmpl.RIGHT_DELIMITER;
+				var htmlCode = {
+					"'" : "&apos;",
+					"\"" : "&quot;",
+					"&" : "&amp;",
+					">" : "&gt;",
+					"<" : "&lt;",
+					"\\" : "&#92;",
+					"/" : "&#47;"
+				}
+				
+				return String(str == null ? "" : str).replace(/['"<>&\/\\]/g, function($0){
+					return 	htmlCode[$0]||"";
+				});
+			}
+			
+			// 正则编码
+			jtmpl.encodeRegExp = function(str){
+				return String(str).replace(/([.*+?^=!:${}()|[\]/\\])/g,'\\$1');	
+			}
+			// 清除换行
+			jtmpl.encodeBr = function(str){
+				return 	String(str == null ? "" : str).replace(/[\t\n\r\v]/g, "");
+			}
+			
+			// 清除注释
+			jtmpl.clearAnnotation = function (str){
+					return String(str)
+							.replace(new RegExp(Ler + "??\\s*?//.*?\\s*?" + Rer,"g"), "")
+							.replace(new RegExp("<!--.*?-->", "g"),"")
+							.replace(new RegExp(Ler+"\\*.*?\\*"+Rer, "g"),"");	
+			}
+			
+			// 模板编译
+			jtmpl.compile = function(str, data){
+				var _rst_fn;
+				
+				// 缓存掉编译结果，省略掉了模板再次加载编译过程
+				if (jtmpl.cache[str]){
+					_rst_fn = jtmpl.cache[str];	
+				}else {
+					var fbody = "var _fbody_arr = [];"
+							  + "\nvar _fn = (function(_data_obj){"
+							  + "\nwith(_data_obj){\n_fbody_arr.push('"
+							  + _javascript(str) 
+							  + "');\n}\n}(data));"
+							  + "\nfn=null;\nreturn _fbody_arr.join('');";
+	
+					_rst_fn = new Function('data', fbody);
+					jtmpl.cache[str] = _rst_fn;
+				}
+				// 如果传入数据，返回编辑完的html,否则返回模板编译函数
+				return /\[object\s+(?:Function|Object)\]/g.test(Object.prototype.toString.call(data)) ? _rst_fn(data) : _rst_fn;
+			}
+			
+			// 处理子模板
+			jtmpl.include = function(str){
+				return String(str == null ? "" : str)
+						.replace(new RegExp(Ler + '\\s*?p(?:art)?\\s*?\\:\\s*?([\\w-]+)\\s*?' + Rer,'g'), function(str, id){
+							return 	jtmpl.load('#' + id);
+						});
+			}
+			
+			// 加载模板格式
+			jtmpl.load = function(str, callback){
+				var refer, elem, html, cache = jtmpl.cache;
 
-    if (!String.prototype.trim) {
-        String.prototype.trim = function() {
-            return this.replace(/^[\s\xa0]+|[\s\xa0]+$/g, '');
-        }
-    }
+				if (refer = (/f(?:ile)?\:([A-Za-z0-9\u4E00-\u9FA5/?&.:=""]+)/.exec(str)||[])[1]){
+					// 外部模板需要加载，异步执行回调
+					callback(refer);	
+				}else if (refer = (/#([\w-$]+)/.exec(str)||[])[1]){
+					elem = document.getElementById(refer);
+					
+					html = jtmpl.include(elem && elem.tagName && /input|textarea/ig.test(elem.tagName) 
+								? elem.value : elem.innerHTML);
 
-    var _type = function(obj) {
-        return /\[object\s+(\w+)/.exec(Object.prototype.toString.call(obj).toLowerCase())[1];
-    }
-
-    var isObject = function(o) {
-        var rst = Object.prototype.toString.call(o);
-        return rst == '[object Function]' || rst == '[object Object]';
-    }
-
-    // 编译模板并传递数据
-    jtmpl.template = function(tmpl, data) {
-        if (!tmpl) {
-            return;
-        }
-
-        var str = jtmpl.load(tmpl),
-        rst = jtmpl.compile((str || '').trim(), data);
-
-        return rst;
-    }
-
-    // html过滤
-    jtmpl.encodeHTML = function(str) {
-
-        var htmlCode = {
-            "'": "&apos;",
-            "\"": "&quot;",
-            "&": "&amp;",
-            ">": "&gt;",
-            "<": "&lt;",
-            "\\": "&#92;",
-            "/": "&#47;"
-        }
-
-        return String(str == null ? "": str).replace(/['"<>&\/\\]/g,
-        function($0) {
-            return htmlCode[$0] || "";
-        });
-    }
-
-    // 正则编码
-    jtmpl.encodeRegExp = function(str) {
-        return String(str).replace(/([.*+?^=!:${}()|[\]/\\]) / g,
-        '\\$1');
-    }
-    // 清除换行
-    jtmpl.encodeBr = function(str) {
-        return String(str == null ? "": str).replace(/[\t\n\r\v]/g, "");
-    }
-
-    // 清除注释
-    jtmpl.clearAnnotation = function(str) {
-        return String(str).replace(new RegExp(Ler + "??\\s*?//.*?\\s*?" + Rer, "g"), "").replace(new RegExp("<!--.*?-->", "g"), "").replace(new RegExp(Ler + "\\*.*?\\*" + Rer, "g"), "");
-    }
-
-    // 模板编译
-    jtmpl.compile = function(str, data) {
-        var _rst_fn;
-
-        // 缓存掉编译结果，省略掉了模板再次加载编译过程
-        if (jtmpl.cache[str]) {
-            _rst_fn = jtmpl.cache[str];
-        } else {
-            var fbody = "var _fbody_arr = [];" + "\nvar _fn = (function(_data_obj){" + "\nwith(_data_obj){\n_fbody_arr.push('" + _javascript(str) + "');\n}\n}(data));" + "\nfn=null;\nreturn _fbody_arr.join('');";
-
-            _rst_fn = new Function('data', fbody);
-            jtmpl.cache[str] = _rst_fn;
-        }
-        // 如果传入数据，返回编辑完的html,否则返回模板编译函数
-        return /\[object\s+(?:Function|Object)\]/g.test(Object.prototype.toString.call(data)) ? _rst_fn(data) : _rst_fn;
-    }
-
-    // 处理子模板
-    jtmpl.include = function(str) {
-        return String(str == null ? "": str).replace(new RegExp(Ler + '\\s*?p(?:art)?\\s*?\\:\\s*?([\\w-]+)\\s*?' + Rer, 'g'),
-        function(str, id) {
-            return jtmpl.load('#' + id);
-        });
-    }
-
-    // 加载模板格式
-    jtmpl.load = function(str, callback) {
-        var refer, elem, html, cache = jtmpl.cache;
-
-        if (refer = (/f(?:ile)?\:([A-Za-z0-9\u4E00-\u9FA5/ ? &.: =""] + ) / .exec(str) || [])[1]) {
-            // 外部模板需要加载，异步执行回调
-            callback(refer);
-        } else if (refer = (/#([\w-$]+)/.exec(str) || [])[1]) {
-            elem = document.getElementById(refer);
-
-            html = jtmpl.include(elem && elem.tagName && /input|textarea/ig.test(elem.tagName) ? elem.value: elem.innerHTML);
-
-            return html;
-        } else {
-            return str;
-        }
-    }
-
-    /**
+					return html;
+				}else {
+					return str;	
+				}
+			}
+			
+			/**
 			* 直接渲染到容器
 			* @ str 模板id
 			* @ el目标容器
 			* @ data数据
 			* @ 其实处理方式，例如jq可以用回调处理
 			**/
-    jtmpl.render = function(tmpl, el, data, callback) {
-        if (!tmpl || typeof(tmpl) != 'string' || !el || !data || !isObject(data)) {
-            return;
-        }
+			jtmpl.render = function(tmpl, el, data, callback){
+				if (!tmpl || typeof(tmpl) != 'string' || !el || !data || !isObject(data)){
+					return;	
+				}
+				
+				var elem = typeof(el) == 'string' ? document.getElementById(el) : el;
+				
+				// 如果是dom元素直接innerHTML,否则交给回调适情况处理
+				if (elem && elem.nodeName){
+					elem.innerHTML = jtmpl.compile(tmpl, data);	
+				}else if (!!callback){
+					callback(elem, jtmpl.compile(tmpl, data));	
+				}
+			}
+			
+			var _javascript = function(str){
+				// 清除注释和换行
+				str = jtmpl.encodeBr(jtmpl.clearAnnotation(str));
+				str = str.replace(new RegExp("("+Ler+"\\s*?[html]*?\:??\\s*?\=\\s*?[^;|"+Rer+"]+?\\s*?)" + Rer, 'g'), "$1\;" + Rer)
+						 .replace(new RegExp(Ler + '\\s*?h(?:tml)??\:\\s*?\=\\s*?([^;|'+Rer+']+?);?\\s*?' + Rer, 'g'), "',typeof($1) === 'undefined' ? '' : jtmpl.encodeHTML($1),'")
+						 .replace(new RegExp(Ler + '\\s*?\=\\s*?([^;|'+Rer+']+?);\\s*?' + Rer, 'g'), "',typeof($1) === 'undefined' ? '' : $1,'");	
 
-        var elem = typeof(el) == 'string' ? document.getElementById(el) : el;
-
-        // 如果是dom元素直接innerHTML,否则交给回调适情况处理
-        if (elem && elem.nodeName) {
-            elem.innerHTML = jtmpl.compile(tmpl, data);
-        } else if ( !! callback) {
-            callback(elem, jtmpl.compile(tmpl, data));
-        }
-    }
-
-    var _javascript = function(str) {
-        // 清除注释和换行
-        str = jtmpl.encodeBr(jtmpl.clearAnnotation(str));
-        str = str.replace(new RegExp("(" + Ler + "\\s*?[html]*?\:??\\s*?\=\\s*?[^;|" + Rer + "]+?\\s*?)" + Rer, 'g'), "$1\;" + Rer).replace(new RegExp(Ler + '\\s*?h(?:tml)??\:\\s*?\=\\s*?([^;|' + Rer + ']+?);?\\s*?' + Rer, 'g'), "',typeof($1) === 'undefined' ? '' : jtmpl.encodeHTML($1),'").replace(new RegExp(Ler + '\\s*?\=\\s*?([^;|' + Rer + ']+?);\\s*?' + Rer, 'g'), "',typeof($1) === 'undefined' ? '' : $1,'");
-
-        return str.split(Ler).join("');").split(Rer).join("_fbody_arr.push('");
-    };
-
-    jtmpl._javascript = _javascript;
-
-    return jtmpl;
-},window));
+				return str.split(Ler).join("');").split(Rer).join("_fbody_arr.push('");
+			};
+			
+			jtmpl._javascript = _javascript;
+			
+			return jtmpl;	
+		}, window));
